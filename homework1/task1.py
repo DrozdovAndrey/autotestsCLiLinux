92abe202c91a4f2f0d3b8f1615487143a9914fba
DrozdovAndrey <DrozdovAndrey@github.com>
2024-05-30 10:54:06 +0300

    added some changes

--- homework1/task1.py
+++ homework1/task1.py
@@ -2,11 +2,14 @@ import subprocess
 
 
 def check_returncode_and_text(coman_text: str) -> bool:
-    result = subprocess.run('cat /etc/os-release', shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    result = subprocess.run(coman_text, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
     out = result.stdout
     if result.returncode == 0:
         lst = out.split('\n')
-        if 'PRETTY_NAME="Ubuntu 22.04.1 LTS"' in lst and 'VERSION="22.04.1 LTS (Jammy JellyFish)"' in lst:
+        if 'PRETTY_NAME="Ubuntu 22.04.1 LTS"' in lst and 'VERSION="22.04.1 LTS (Jammy Jellyfish)"' in lst:
             return True
         else:
             return False
+
+
+print(check_returncode_and_text('cat /etc/os-release'))
