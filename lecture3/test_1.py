6efd1dfcd15730afcb339f717c3be9c182a785df
DrozdovAndrey <DrozdovAndrey@github.com>
2024-06-02 15:31:50 +0300

    added flag -t

--- lecture3/test_1.py
+++ lecture3/test_1.py
@@ -9,14 +9,14 @@ with open('config.yaml') as f:
 class TestPositive:
     def test_step1(self, make_folders, clear_folders, make_files):
         # test1
-        res1 = checkout(f"cd {data['folder_in']}; 7z a {data['folder_out']}/arx", "Everything is Ok")
+        res1 = checkout(f"cd {data['folder_in']}; 7z a {data['type']} {data['folder_out']}/arx", "Everything is Ok")
         res2 = checkout(f"ls {data['folder_out']}", "arx.7z")
         assert res1 and res2, "test1 FAIL"
 
     def test_step2(self, clear_folders, make_files):
         # test2
         res = list()
-        res.append(checkout(f"cd {data['folder_in']}; 7z a {data['folder_out']}/arx", "Everything is Ok"))
+        res.append(checkout(f"cd {data['folder_in']}; 7z a {data['type']} {data['folder_out']}/arx", "Everything is Ok"))
         res.append(
             checkout(f"cd {data['folder_out']}; 7z e arx.7z -o{data['folder_ext']} -y", "Everything is Ok"))
         for item in make_files:
@@ -34,7 +34,7 @@ class TestPositive:
     def test_step5(self, clear_folders, make_files):
         # test5
         res = list()
-        res.append(checkout(f"cd {data['folder_in']}; 7z a {data['folder_out']}/arx", "Everything is Ok"))
+        res.append(checkout(f"cd {data['folder_in']}; 7z a {data['type']} {data['folder_out']}/arx", "Everything is Ok"))
         for i in make_files:
             res.append(checkout(f"cd {data['folder_out']}; 7z l arx.7z", i))
         assert all(res), "test5 FAIL"
@@ -42,7 +42,7 @@ class TestPositive:
     def test_step6(self, clear_folders, make_files, make_subfolder):
         # test6
         res = list()
-        res.append(checkout(f"cd {data['folder_in']}; 7z a {data['folder_out']}/arx", "Everything is Ok"))
+        res.append(checkout(f"cd {data['folder_in']}; 7z a {data['type']} {data['folder_out']}/arx", "Everything is Ok"))
         res.append(
             checkout(f"cd {data['folder_out']}; 7z x arx.7z -o{data['folder_ext2']} -y", "Everything is Ok"))
         for i in make_files:
